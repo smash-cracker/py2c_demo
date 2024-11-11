95fe2eed3df215070cc70d06564f527cf7721ec1
smash-cracker <dennythomas069@gmail.com>
2024-11-11 11:30:18 +0530

    data transfer

--- backend/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.c
+++ backend/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.c
@@ -2,42 +2,75 @@
 #include <string.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#define ARRAY_SIZE 3  
+#define MAX_STRING_LEN 20  
 #pragma comment(lib, "ws2_32.lib")
+
 typedef struct {
-    unsigned char head;
-    unsigned char sector;
-    unsigned char cyl;
-} Position;
+    char stringArray[ARRAY_SIZE][MAX_STRING_LEN]; 
+    char singleString[MAX_STRING_LEN];            
+    int number;                                   
+} Data;
+
+void decodeData(Data *data, unsigned char *buffer) {
+    unsigned char *ptr = buffer;
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        memcpy(data->stringArray[i], ptr, MAX_STRING_LEN);
+        ptr += MAX_STRING_LEN;
+    }
+    memcpy(data->singleString, ptr, MAX_STRING_LEN);
+    ptr += MAX_STRING_LEN;
+    memcpy(&data->number, ptr, sizeof(int));
+}
+
+void displayData(Data *data) {
+    printf("Array of Strings:\n");
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        // Print each string, making sure to ignore the null-padding at the end
+        printf("  String %d: \"%s\"\n", i + 1, data->stringArray[i]);
+    }
+    printf("Single String: \"%s\"\n", data->singleString);
+    printf("Integer: %d\n", data->number);
+}
+
 int main() {
     WSADATA wsaData;
     int server_fd, client_fd;
     struct sockaddr_in server_addr, client_addr;
     socklen_t addr_len = sizeof(client_addr);
+
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         printf("WSAStartup failed\n");
         return 1;
     }
+
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd == INVALID_SOCKET) {
-        printf("Socket failed\n");
+        printf("Socket creation failed\n");
         WSACleanup();
         return 1;
     }
+
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(8080);
+
     if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
         printf("Bind failed\n");
         closesocket(server_fd);
         WSACleanup();
         return 1;
     }
+
     if (listen(server_fd, 1) == SOCKET_ERROR) {
         printf("Listen failed\n");
         closesocket(server_fd);
         WSACleanup();
         return 1;
     }
+
+    printf("Waiting for client connection...\n");
+
     client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &addr_len);
     if (client_fd == INVALID_SOCKET) {
         printf("Accept failed\n");
@@ -45,18 +78,41 @@ int main() {
         WSACleanup();
         return 1;
     }
-    Position pos;
-    int bytes_received = recv(client_fd, (char*)&pos, sizeof(Position), 0);
-    if (bytes_received == SOCKET_ERROR) {
-        printf("Receive failed\n");
-        closesocket(client_fd);
-        closesocket(server_fd);
-        WSACleanup();
-        return 1;
+
+    printf("Client connected.\n");
+
+    while (1) {
+        // Define the data struct and receive data in a loop
+        Data received_data;
+        int bytes_received = recv(client_fd, (char*)&received_data, sizeof(Data), 0);
+
+        if (bytes_received == SOCKET_ERROR) {
+            printf("Receive failed\n");
+            break;
+        }
+        else if (bytes_received == 0) {
+            printf("Client disconnected\n");
+            break;
+        }
+
+        // Print the received data
+        printf("Received new data:\n");
+
+        // Display the integer field correctly
+        printf("Integer field: %d\n", received_data.number);
+
+        // Print each string in the array of strings
+        displayData(&received_data);
+
+        // Optional: Send an acknowledgment back to the client
+        const char* ack_msg = "Data received successfully";
+        send(client_fd, ack_msg, strlen(ack_msg), 0);
     }
-    printf("Head: %d, Sector: %d, Cyl: %d\n", pos.head, pos.sector, pos.cyl);
+
+    // Close sockets and clean up
     closesocket(client_fd);
     closesocket(server_fd);
     WSACleanup();
+
     return 0;
 }
--- frontend/send.py
+++ frontend/send.py
@@ -1,16 +1,10 @@
 import sys
 import socket
-import cstruct
-from PyQt5.QtCore import QCoreApplication, QThread, pyqtSignal
-class Position(cstruct.MemCStruct):
-    __byte_order__ = cstruct.LITTLE_ENDIAN
-    __def__ = """
-        struct {
-            unsigned char head;
-            unsigned char sector;
-            unsigned char cyl;
-        }
-    """
+import struct
+ARRAY_SIZE = 3            
+MAX_STRING_LEN = 20   
+from PyQt5.QtCore import QThread
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLineEdit, QPushButton, QSpinBox, QLabel, QListWidget, QInputDialog
 class TcpClientThread(QThread):
     def __init__(self, packed_data, host, port):
         super().__init__()
@@ -22,13 +16,74 @@ class TcpClientThread(QThread):
         client_socket.connect((self.host, self.port))
         client_socket.sendall(self.packed_data)
         client_socket.close()
-class MyApp(QCoreApplication):
-    def __init__(self, packed_data, host, port):
-        super().__init__(sys.argv)
-        self.client_thread = TcpClientThread(packed_data, host, port)
+class MyApp:
+    def __init__(self):
+        self.app = QApplication(sys.argv)
+        self.ui = MyWindow()
+        self.ui.show()
+    def start_client_thread(self, packed_data):
+        self.client_thread = TcpClientThread(packed_data, 'localhost', 8080)
         self.client_thread.start()
+class MyWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Data Input UI")
+        self.setGeometry(100, 100, 400, 300)
+        self.layout = QVBoxLayout()
+        self.integer_label = QLabel("Enter an integer:")
+        self.integer_input = QSpinBox()
+        self.layout.addWidget(self.integer_label)
+        self.layout.addWidget(self.integer_input)
+        self.string_label = QLabel("Enter a string:")
+        self.string_input = QLineEdit()
+        self.layout.addWidget(self.string_label)
+        self.layout.addWidget(self.string_input)
+        self.array_label = QLabel("Enter an array of strings (one per line):")
+        self.array_input = QListWidget()
+        self.layout.addWidget(self.array_label)
+        self.layout.addWidget(self.array_input)
+        self.array_input.addItem("Item 1")
+        self.array_input.addItem("Item 2")
+        self.add_button = QPushButton("Add Item")
+        self.add_button.clicked.connect(self.add_item)
+        self.layout.addWidget(self.add_button)
+        self.delete_button = QPushButton("Delete Selected Item")
+        self.delete_button.clicked.connect(self.delete_item)
+        self.layout.addWidget(self.delete_button)
+        self.send_button = QPushButton("Send Data")
+        self.send_button.clicked.connect(self.send_data)
+        self.layout.addWidget(self.send_button)
+        self.setLayout(self.layout)
+    def add_item(self):
+        text, ok = QInputDialog.getText(self, "Add Item", "Enter a string:")
+        if ok and text:
+            self.array_input.addItem(text)
+    def delete_item(self):
+        selected_item = self.array_input.currentRow()
+        if selected_item != -1:
+            self.array_input.takeItem(selected_item)
+    def pad_string(self, s, length):
+        return s.encode('utf-8').ljust(length, b'\x00')[:length]
+    def send_data(self):
+        integer_value = self.integer_input.value()
+        string_value = self.string_input.text()
+        array_of_strings = []
+        for i in range(min(self.array_input.count(), 5)):
+            array_of_strings.append(self.array_input.item(i).text())
+        data = {
+            'string_array': array_of_strings,
+            'single_string': string_value,
+            'number': integer_value
+        }
+        format_string = f'{ARRAY_SIZE * MAX_STRING_LEN}s{MAX_STRING_LEN}sI'
+        packed_data = b''
+        for s in data['string_array']:
+            packed_data += self.pad_string(s, MAX_STRING_LEN)
+        packed_data += self.pad_string(data['single_string'], MAX_STRING_LEN)
+        packed_data += struct.pack('I', data['number'])
+        print(packed_data)
+        app.start_client_thread(packed_data)
+
 if __name__ == '__main__':
-    pos = Position(head=10, sector=20, cyl=3)
-    packed = pos.pack()
-    app = MyApp(packed, 'localhost', 8080)
-    sys.exit(app.exec_())
+    app = MyApp()
+    sys.exit(app.app.exec_())
